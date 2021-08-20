40df607129d3bebb9462659171150004a509981a
koji-torishima <torikou3685@gmail.com>
2021-08-21 00:36:07 +0900

    画面を作成した

--- SampleApp1/app/src/main/java/com/example/sampleapp1/MainActivity.kt
+++ SampleApp1/app/src/main/java/com/example/sampleapp1/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.sampleapp1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    fun sendMassage(view: View) {
     }
 }
