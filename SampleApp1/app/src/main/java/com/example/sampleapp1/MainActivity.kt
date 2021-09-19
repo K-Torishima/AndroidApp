d2efbc4e8ecb2838c00649c1dab39cfb35032d77
koji-torishima <torikou3685@gmail.com>
2021-09-19 19:03:29 +0900

    コミット

--- SampleApp1/app/src/main/java/com/example/sampleapp1/MainActivity.kt
+++ SampleApp1/app/src/main/java/com/example/sampleapp1/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
+// 続きはこちらから
+// https://developer.android.com/training/basics/firstapp/starting-activity?hl=ja
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
